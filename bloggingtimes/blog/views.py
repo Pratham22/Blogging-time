92e03ec5de6cbc7d9634e0b59182d9159a8e9271
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2020-08-08 23:25:58 +0530

    setting up the base

---
+++ bloggingtimes/blog/views.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, View
+from django.shortcuts import redirect
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+def home(request):
+    return render(request,'index.html')
