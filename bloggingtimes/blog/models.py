397f5f8763436583cf7859f49e39b775c7ff09c3
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2020-08-16 17:29:07 +0530

    register.html set up

--- bloggingtimes/blog/models.py
+++ bloggingtimes/blog/models.py
@@ -8,8 +8,11 @@ class Post(models.Model):
     main_img=models.ImageField(null=True)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    author = models.ForeignKey(User,related_name='author_of_the_blog', on_delete=models.CASCADE)
+    ##Needs to be set
+    #approvedBy = models.ForeignKey(User,related_name='approver_of_the_block',on_delete=models.CASCADE)
+    #approved=models.BooleanField(default=False)
+    
     def __str__(self):
         return self.title
 '''
