e012f50b0c9e901919ea5610e28cfaf8d092ffd3
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2020-08-24 10:38:35 +0530

    added django tool bar

--- bloggingtimes/bloggingtimes/urls.py
+++ bloggingtimes/bloggingtimes/urls.py
@@ -23,7 +23,15 @@ urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
     path('users/',include('users.urls', namespace='users')),
 ]
+'''
 if settings.DEBUG:
     urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+'''
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
