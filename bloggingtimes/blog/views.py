3a0f3c4e9151d0d0142291043af430c1d143304a
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2020-09-14 02:34:16 +0530

    with some added time

--- bloggingtimes/blog/views.py
+++ bloggingtimes/blog/views.py
@@ -10,6 +10,7 @@ from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from .models import Post,comment
 from .forms import cmtform,blogform,newblog
+from django.http import HttpResponse
 class home(View):
     def get(self, *args, **kwargs):
         post=Post.objects.order_by('-date_posted').filter(passed_by_mentor=True)
@@ -18,13 +19,46 @@ class home(View):
         }
         return render(self.request, "index.html", context)
 
-class Postcreateview(LoginRequiredMixin,CreateView):
+#class Postcreateview(CreateView):
+    '''
     model=Post
     fields=['title','content','thumnail_image','main_img']
     def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
+'''
+@login_required
+def Postcreateview(request):
+    return render(request,'new-blog-post.html')
+
+@login_required
+def new_all_blogs(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            title = request.POST['title']
+            blogContent = request.POST['blog-content']
+            main_img=request.POST['cover-images']
+            thum_img=request.POST['thum-images']
+            try:
+                user_logged=request.user
+               
+            except:
+                return HttpResponse('user logged in not detected')
+            try:
+                #user1 = Post(author=request.user,title=title,main_img='',thumnail_img='')
+                #print(user1)
+                create_blog=Post.objects.create(
+                    title=title,content=blogContent,author=user_logged,main_img=main_img,thumnail_img=thum_img
+                )
+            except:
+                return HttpResponse('Error in object create')
+            #try:
+             #   user.save() 
+            #except:
+             #   return HttpResponse('error in save')
+            return HttpResponse('test.html')
 
 class all_blogs(ListView):
     model = Post
