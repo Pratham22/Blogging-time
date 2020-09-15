ebcfbe19a812db8cbf00aa8e1a710d12447dc48b
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2020-09-16 00:19:33 +0530

    blog creation

--- bloggingtimes/blog/urls.py
+++ bloggingtimes/blog/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home,PostDetailView,all_blogs,create_blog
+from .views import home,new_all_blogs,PostDetailView,all_blogs,Postcreateview
 app_name = 'blog'
 urlpatterns = [
     path('',home.as_view(),name="home"),
     path('blogs',all_blogs.as_view(),name="all-blog"),
     path('post/<int:pk>/', PostDetailView, name='post-detail'),
-    path('createblog/',create_blog.as_view(),name="create-blog"),
+    #path('createblog/',Postcreateview.as_view(),name="create-blog"),
+    path('createblog',Postcreateview,name="Postcreateview"),
+    path('new_all_blogs',new_all_blogs,name="new_all_blogs")
 ]
--- bloggingtimes/blog/views.py
+++ bloggingtimes/blog/views.py
@@ -2,14 +2,16 @@ from django.conf import settings
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, View
+from django.views.generic import ListView, DetailView, View,CreateView
 from django.shortcuts import redirect
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from .models import Post,comment
 from .forms import cmtform
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 class home(View):
     def get(self, *args, **kwargs):
         post=Post.objects.order_by('-date_posted').filter(passed_by_mentor=True)
@@ -18,9 +20,27 @@ class home(View):
         }
         return render(self.request, "index.html", context)
 
-class create_blog(View):
-    def get(self, *args, **kwargs):
-        return render(self.request, "new-blog-post.html")
+@login_required
+def Postcreateview(request):
+    return render(request,'new-blog-post.html')
+
+@login_required
+def new_all_blogs(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            title = request.POST['title']
+            blogcontent = request.POST.get('content')
+            main_img=request.FILES['coverimages']
+            thum_img=request.FILES['thumimages']
+            fs=FileSystemStorage()
+            filename=fs.save(main_img.name,main_img)
+            filename2=fs.save(thum_img.name,thum_img)
+            user_logged=request.user               
+            Post.objects.create(author=user_logged,title=title,content=blogcontent,main_img=main_img,thumnail_image=thum_img)
+            return HttpResponse('your blog is under approval of the mentor and will be available on the list after approval')
+    else:
+        return HttpResponse('login.html')
+
 class all_blogs(ListView):
     model = Post
     template_name = 'all-blogs-page.html'  
@@ -36,10 +56,12 @@ class all_blogs(ListView):
        else:
            result = Post.objects.order_by('-date_posted')
        return result
+
+
 def PostDetailView(request,pk):
     template_name='single-blog-page.html'
     post=get_object_or_404(Post,id=pk)
-    comments=comment.objects.filter(active=True).filter(post=post)
+    comments=comment.objects.filter(active=True).filter(post=post).order_by('-created_on')
     if request.method=='POST':
         comment_form=cmtform(request.POST or None)
         try:
